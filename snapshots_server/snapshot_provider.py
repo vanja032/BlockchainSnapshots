9a5c774be054c82d384b740ccbcf7f04e0d380aa
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2023-03-08 14:59:10 +0100

    Update snapshot_provider.py

--- snapshots_server/snapshot_provider.py
+++ snapshots_server/snapshot_provider.py
@@ -15,12 +15,13 @@ file.close()
 
 snap_location = configs['snapshot_path']
 snap_dir = configs['snap_dir']
-snap_file = configs['snapshot_file']
-snap_file_ext = configs['snapshot_file_extension']
+snapshot = ""
+#snap_file = configs['snapshot_file']
+#snap_file_ext = configs['snapshot_file_extension']
 webpage_address = configs['snapshot_webpage']
 
 favicon = 'favicon.png'
-file_name_ext = ""
+#file_name_ext = ""
 
 app = Flask(__name__)
 CORS(app)
@@ -30,8 +31,10 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 #@cross_origin()
 def ProvideSnapshot():
     try:
-        global file_name_ext
-        return render_template('index.html', snapshot_file_download = '/snapshot', snapshot_url = f'--no-check-certificate {webpage_address}/snapshot -o', snapshot_out = f'{snap_file}_{file_name_ext}{snap_file_ext}', favicon_url = f'/{favicon}')
+        #global file_name_ext
+        global snapshot
+        #return render_template('index.html', snapshot_file_download = '/snapshot', snapshot_url = f'--no-check-certificate {webpage_address}/snapshot -o', snapshot_out = f'{snap_file}_{file_name_ext}{snap_file_ext}', favicon_url = f'/{favicon}')
+        return render_template('index.html', snapshot_file_download = '/snapshot', snapshot_url = f'--no-check-certificate {webpage_address}/snapshot -o', snapshot_out = f'{snapshot}', favicon_url = f'/{favicon}')
         #return "<h1>Hello!</h1>"
     except Exception as ex:
         print(ex)
@@ -40,7 +43,10 @@ def ProvideSnapshot():
 #@cross_origin()
 def DownloadSnapshot():
     try:
-        return send_file(f'{snap_dir}/{snap_file}_{file_name_ext}{snap_file_ext}', as_attachment=True)
+        #global file_name_ext
+        global snapshot
+        #return send_file(f'{snap_dir}/{snap_file}_{file_name_ext}{snap_file_ext}', as_attachment=True)
+        return send_file(f'{snap_dir}/{snapshot}', as_attachment=True)
     except Exception as ex:
         print(ex)
 
@@ -48,8 +54,10 @@ def DownloadSnapshot():
 #@cross_origin()
 def ReturnSnapshotVersion():
     try:
-        global file_name_ext
-        return f'{snap_file}_{file_name_ext}{snap_file_ext}'
+        #global file_name_ext
+        global snapshot
+        #return f'{snap_file}_{file_name_ext}{snap_file_ext}'
+        return f'{snapshot}'
     except Exception as ex:
         print(ex)
 
@@ -57,13 +65,15 @@ def ReturnSnapshotVersion():
 #@cross_origin()
 def ReturnFavicon():
     try:
+        global favicon
         return send_file(f'{favicon}', as_attachment=True)
     except Exception as ex:
         print(ex)
 
 def ExportSnapshot():
     try:
-        global file_name_ext
+        #global file_name_ext
+        global snapshot
         print("\nExporting snapshot...\n" + str(datetime.now()))
         os.system('rm -rf ' + snap_location + '/*')
         command = 'curl -X POST http://127.0.0.1:8888/v1/producer/create_snapshot'
@@ -71,7 +81,9 @@ def ExportSnapshot():
         file_name_ext = str(time.time())
         time.sleep(2)
         os.system('rm -rf ' + snap_dir + '/*')
-        shutil.copy(snap_location + '/' + os.listdir(snap_location)[0], f'{snap_dir}/{snap_file}_{file_name_ext}{snap_file_ext}')
+        #shutil.copy(snap_location + '/' + os.listdir(snap_location)[0], f'{snap_dir}/{snap_file}_{file_name_ext}{snap_file_ext}')
+        snapshot = os.listdir(snap_location)[0]
+        shutil.copy(snap_location + '/' + snapshot, f'{snap_dir}/{snapshot}')
         print("\nExporting snapshot finished.\n" + str(datetime.now()))
         #time.sleep(600) # every 10 minutes export a snapshot
     except Exception as ex:
